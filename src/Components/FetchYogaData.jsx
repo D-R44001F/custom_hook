b5ce0e6de2b111b55f0b596e6dad2a3c897de993
D-R44001F <rodz.dylan44@gmail.com>
2024-10-21 09:07:48 -0400

    added fetch yoga

--- src/Components/FetchYogaData.jsx
+++ src/Components/FetchYogaData.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 import UseFetch from './UseFetch';
 
 const FetchYogaData = () => {
-    const [data]=UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
+    const [data]=UseFetch("https://api.npoint.io/4459a9a10e43812e1152");
     return (
         <>
-        
+            <ul className='list_data_main'>
+                <h1 className='usefetch_heading'>Use Fetch Custom Hook Yoga</h1>
+                {data && data.map((e)=>( // e parameter contains the value of each array index for every iteration
+                    <>
+                        <li className='list_data'>
+                            <h3>{e.name}</h3>
+                            <p><strong>Name: </strong>{e.name}</p>
+                            <p><strong>Benefits: </strong>{e.benefits}</p>
+                            <p><strong>Time: </strong>{e.time}</p>
+                        </li>
+                    </>
+                ))}
+            </ul>
         </>
     )
 }
